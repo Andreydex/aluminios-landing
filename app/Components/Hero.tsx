d1567498bde04b6f96169937c8534212adb5d811
Andreydex <pv230110149@vallarta.tecmm.edu.mx>
2026-03-11 20:57:07 -0600

    Inicio de proyecto: Limpieza y componente Hero

---
+++ app/Components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+  <section className="p-20 bg-gray-800 text-white text-center">
+    <h1 className="text-4xl font-bold">Aluminios y Ventanas</h1>
+    <p className="mt-4 text-xl">El mejor servicio de Puerto Vallarta</p>
+  </section>
+  );
+}
